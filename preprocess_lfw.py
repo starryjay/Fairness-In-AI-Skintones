6b54b1340c7b4ea3e5bcfdffa0ccb71e5456a1cd
starryjay <sshree@usc.edu>
2025-02-25 17:48:07 -0800

    Resizing images

--- preprocess_lfw.py
+++ preprocess_lfw.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd 
 import os
 import shutil
+import tensorflow as tf
 
 
 def create_file_structure():
     '''file structure'''
-    source = 'data/lfw-deepfunneled'
+    source = 'data/lfw-deepfunneled/'
     dest = 'data/'
     img = []
 
@@ -35,18 +36,28 @@ def create_file_structure():
 def preprocess_lfw(lfw_csv):
     '''preprocess lfw csv'''
     df = pd.read_csv(lfw_csv)
-    #get unique persons
-    persons = df['person'].unique()
-    print(len(df))
-    print("Number of persons: ", len(persons))
-
     #modify df to have one img per person randomly
     one_per_person = df.groupby('person').sample(n=1, random_state=42)
-    print("One image per person", len(one_per_person))
-
+    # convert to 4d tensor [batch, height, width, channels]
+    # get image path
+    img_path = one_per_person['file']
+    img_path = img_path.apply(lambda x: os.path.join('data', x))
+    resized = pd.DataFrame(columns=['img'])
+    for img in img_path:
+        #read image
+        img = tf.io.read_file(img)
+        #decode image
+        img = tf.image.decode_jpeg(img, channels=3)
+        #resize image to 224x224
+        img = tf.image.resize(img, [128, 128])
+        #normalize image
+        img = img / 255.0
+        #add to df
+        resized = pd.concat([resized, pd.DataFrame({'img': [img]})], ignore_index=True)
+    return resized
 
 def main():
     #create_file_structure()
-    preprocess_lfw('data/lfw.csv')
+    resized = preprocess_lfw('data/lfw.csv')
 
 main()
