2c12eae54e9224fabceb89f4681a8220f88b1b87
starryjay <sshree@usc.edu>
2025-04-04 15:59:49 -0700

    Updated plots

--- results_two.py
+++ results_two.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from itertools import product
 import matplotlib.patches as mpatches
+from scipy.interpolate import make_interp_spline
 
 
 fashion_data = pd.read_csv('./intermediate_files/closest_images_fashion.csv')
@@ -42,8 +43,10 @@ def plot_results(morphe_df: pd.DataFrame, clustered_df: pd.DataFrame, title: str
     clustered_counts = clustered_df.apply(pd.Series.value_counts).fillna(0).sum(axis=1)
 
     # Ensure both have the same categories
-    all_categories = sorted(set(morphe_counts.index).union(set(clustered_counts.index)))
+    all_categories = ['fairlight', 'light', 'lightmedium', 'medium', 'tan', 'deeptan', 'rich', 'deeprich', 'deep', 'deepest']
     morphe_counts = morphe_counts.reindex(all_categories, fill_value=0)
+    # sort from light to dark
+
     clustered_counts = clustered_counts.reindex(all_categories, fill_value=0)
 
     # Create side-by-side bar plot
@@ -51,16 +54,16 @@ def plot_results(morphe_df: pd.DataFrame, clustered_df: pd.DataFrame, title: str
     bar_width = 0.4  # Width of each bar
 
     morphe_color_map = {
-        'deep':(0.6392156863,0.3176470588,0.1411764706),
-        'deepest':(0.4196078431,0.1725490196,0.05098039216),
-        'deeprich':(0.8431372549,0.4549019608,0.2156862745),
-        'deeptan':(0.8823529412,0.5411764706,0.3529411765),
-        'fairlight':(0.9450980392,0.7607843137,0.6470588235),
-        'light':(1,0.7490196078,0.6),
-        'lightmedium':(1,0.7647058824,0.5960784314),
-        'medium':(0.9960784314 ,   0.7058823529  , 0.5254901961),
-        'rich':(0.8431372549,0.5176470588,0.3019607843),
-        'tan': (0.968627451,0.6549019608,0.4588235294)
+        'deep':(0.6392156863, 0.3176470588, 0.1411764706),
+        'deepest':(0.4196078431, 0.1725490196, 0.05098039216),
+        'deeprich':(0.8431372549, 0.4549019608, 0.2156862745),
+        'deeptan':(0.8823529412, 0.5411764706, 0.3529411765),
+        'fairlight':(0.9450980392, 0.7607843137, 0.6470588235),
+        'light':(1, 0.7490196078, 0.6),
+        'lightmedium':(1, 0.7647058824, 0.5960784314),
+        'medium':(0.9960784314, 0.7058823529, 0.5254901961),
+        'rich':(0.8431372549, 0.5176470588, 0.3019607843),
+        'tan': (0.968627451, 0.6549019608, 0.4588235294)
     }
 
     clusterd_color_map  = {
@@ -82,11 +85,17 @@ def plot_results(morphe_df: pd.DataFrame, clustered_df: pd.DataFrame, title: str
 
   
     lightform_patch = mpatches.Patch(color='salmon', label='Morphe')
-    clusters_patch = mpatches.Patch(color='springgreen', label='Clustered')
+    clusters_patch = mpatches.Patch(color='palegreen', label='Clustered')
     plt.legend(handles=[lightform_patch, clusters_patch], title = "Dataset")
 
-    plt.bar([pos - bar_width / 2 for pos in x], morphe_counts, bar_width, label='Morphe', color = [morphe_color_map[i] for i in morphe_counts.index],edgecolor= 'salmon', linewidth=3)
-    plt.bar([pos + bar_width / 2 for pos in x], clustered_counts, bar_width, label='Clustered', color = [clusterd_color_map[i] for i in clustered_counts.index], edgecolor = 'springgreen', linewidth=3)
+    #spline_lightform = make_interp_spline(x, morphe_counts, k=5)
+    #spline_clustered = make_interp_spline(x, clustered_counts, k=5)
+    plt.bar([pos - bar_width / 2 for pos in x], morphe_counts, bar_width, label='Morphe', color = [morphe_color_map[i] for i in morphe_counts.index],edgecolor= 'salmon', linewidth=2)
+    #plt.plot(x, spline_lightform(x), color='salmon', linewidth=2)
+    plt.bar([pos + bar_width / 2 for pos in x], clustered_counts, bar_width, label='Clustered', color = [clusterd_color_map[i] for i in clustered_counts.index], edgecolor = 'palegreen', linewidth=2)
+    #plt.plot(x, spline_clustered(x), color='springgreen', linewidth=2)
+    plt.fill_between(x, morphe_counts, color='salmon', alpha=0.3)
+    plt.fill_between(x, clustered_counts, color='palegreen', alpha=0.3)
 
     # Add labels, legend, and title
     plt.xticks(x, all_categories, rotation=45)
